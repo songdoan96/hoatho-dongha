7d3610056d666bf8f7bc3b0d7ce958546d1db46a
songdoan96 <songdoan96@gmail.com>
2024-08-10 16:06:43 +0700

    Schedule

--- src/utils/DateTimeFormat.ts
+++ src/utils/DateTimeFormat.ts
@@ -1,6 +1,6 @@
 import moment from "moment";
 
-export const formatDate = (date: Date | string | undefined, format?: string | nul) => {
+export const formatDate = (date: Date | string | undefined, format?: string | null) => {
   if (date) {
     return moment(new Date(date)).format(format ?? "DD-MM-YYYY");
   }
